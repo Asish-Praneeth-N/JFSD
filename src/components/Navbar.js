d769ea702cf012e509a00c88fe7ec2557f90b95a
Asish-Praneeth-N <142401337+Asish-Praneeth-N@users.noreply.github.com>
2024-11-05 10:31:07 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,44 @@
-import React from 'react';
+// src/components/Navbar.js
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // CSS for styling the navbar
+import './Navbar.css';
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <header className="navbar">
       <div className="navbar-brand">
         <img src={process.env.PUBLIC_URL + '/images/farmpulse.png'} alt="FarmPulse Logo" className="navbar-logo" />
         <h1 className="navbar-name">FarmPulse</h1>
       </div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/resources">Resources</Link></li>
-          <li><Link to="/community">Community</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
+      {!isMobile && (
+        <nav className="navbar-nav">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/resources">Resources</Link></li>
+            <li><Link to="/community">Community</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+      )}
       <div className="navbar-buttons">
-        <Link to="/login">
-          <button className="login-signup">Login</button>
-        </Link>
         <Link to="/signup">
-          <button className="login-signup">Sign Up</button>
+          <button className="login-signup">Login/Signup</button>
         </Link>
+        {isMobile && (
+          <button className="mobile-menu-button">☰</button>
+        )}
       </div>
     </header>
   );
